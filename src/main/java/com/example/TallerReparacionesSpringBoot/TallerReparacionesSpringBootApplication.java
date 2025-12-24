40bbf63e50a8a067b6e57efde96bebf92e6dbb44
CrisMuniz <cristinamuniz85@gmail.com>
2025-12-24 12:07:27 +0100

    Proyecto inicial Spring Boot - Taller Reparaciones

---
+++ src/main/java/com/example/TallerReparacionesSpringBoot/TallerReparacionesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.TallerReparacionesSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerReparacionesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerReparacionesSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TallerReparacionesSpringBoot/TallerReparacionesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TallerReparacionesSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerReparacionesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
